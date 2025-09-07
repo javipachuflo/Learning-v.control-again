5735adfd07cd8b692c3540e453446f42c4ddd655
Javier Alvarez Sanzberro <jasanzberro@gmail.com>
2025-09-07 15:30:32 +0100

    Acceleration + Prototype Textures
    Added acceleration to character controller
    
    inserted prototype textures

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -8,17 +8,43 @@ public class PlayerController1 : MonoBehaviour
     Vector2 moveInput;
     Rigidbody rb;
     [SerializeField] float jumpPower = 5f;
+    [SerializeField] float maxSpeed = 10f;
+    [SerializeField] float acceleration = 20f;
+    [SerializeField] float deceleration = 10f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.AddForce(moveInput.x, 0, moveInput.y); // we can place a Vector3 straight into the method since that's the input it is waiting for (no need for external Vector3s plugged in)
+        Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y) * maxSpeed;
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 velocityChange;
+
+        if (moveInput.magnitude > 0.01f)
+        {
+            // Accelerate towards target velocity
+            velocityChange = Vector3.MoveTowards(
+                new Vector3(velocity.x, 0, velocity.z),
+                new Vector3(targetVelocity.x, 0, targetVelocity.z),
+                acceleration * Time.deltaTime
+            ) - new Vector3(velocity.x, 0, velocity.z);
+        }
+        else
+        {
+            // Decelerate to zero
+            velocityChange = Vector3.MoveTowards(
+                new Vector3(velocity.x, 0, velocity.z),
+                Vector3.zero,
+                deceleration * Time.deltaTime
+            ) - new Vector3(velocity.x, 0, velocity.z);
+        }
+
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+        Debug.Log(moveInput);
     }
 
     void OnJump(InputValue inputValue) {
